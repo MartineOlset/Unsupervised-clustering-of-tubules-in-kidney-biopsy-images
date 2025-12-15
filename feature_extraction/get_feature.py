26d630f17ad944fb6d135a993d9335598182d559
MartineOlset <olsetm@yahoo.no>
2025-12-15 12:30:39 +0100

    add content

---
+++ feature_extraction/get_feature.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import torch, torchvision
+import torch.nn as nn
+from torchvision import transforms, models
+from PIL import Image
+import ResNet as ResNet
+from torch.utils.data import Dataset
+import numpy as np
+
+def trnsfrms_val():
+    mean = (0.485, 0.456, 0.406)
+    std = (0.229, 0.224, 0.225)
+    trnsfrms_val = transforms.Compose(
+        [
+            transforms.Resize(256),
+            transforms.ToTensor(),
+            transforms.Normalize(mean = mean, std = std)
+        ]
+    )
+    return trnsfrms_val
+
+class roi_dataset(Dataset):
+    def __init__(self, img_csv, trnsfrms_val):
+        super().__init__()
+        self.transform = trnsfrms_val
+
+        self.images_lst = img_csv
+
+    def __len__(self):
+        return len(self.images_lst)
+
+    def __getitem__(self, idx):
+        path = self.images_lst.filename[idx]
+        image = Image.open(path).convert('RGB')
+        image = self.transform(image)
+
+        return image
+
+def get_feature(path, trnsfrms_val): 
+    all_features = []
+    img_csv=pd.read_csv(path)
+    test_datat=roi_dataset(img_csv, trnsfrms_val)
+    database_loader = torch.utils.data.DataLoader(test_datat, batch_size=1, shuffle=False)
+
+    model = ResNet.resnet50(num_classes=128,mlp=False, two_branch=False, normlinear=True)
+    pretext_model = torch.load(r'./best_ckpt.pth', map_location='cpu')
+    model.fc = nn.Identity()
+    model.load_state_dict(pretext_model, strict=True)
+
+    model.eval()
+    with torch.no_grad():
+        for batch in database_loader:
+            features = model(batch)
+            features = features.cpu().numpy()
+            all_features.append(features.squeeze(0)) 
+
+    all_features = np.vstack(all_features)  
+   
+    return all_features
